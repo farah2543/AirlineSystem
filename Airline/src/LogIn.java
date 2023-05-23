94eeae5d3c22aad40b3b0a1046582799315087b9
esraamahmoud7 <101475325+esraamahmoud7@users.noreply.github.com>
2023-05-23 10:28:55 +0300

    Add files via upload

---
+++ Airline/src/LogIn.java
@@ -0,0 +1,81 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class LogIn extends JDialog{
+    private JTextField IDP;
+    private JPasswordField passwordField;
+    private JButton logInButton;
+    private JButton cancelButton;
+    private JPanel logP;
+    public boolean user;
+
+    public LogIn(JFrame parent)
+    {
+        super(parent);
+        setTitle("Log In :) ");
+        setContentPane(logP);
+        setMinimumSize(new Dimension(550, 474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        logInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String id=IDP.getText();
+                String pass=String.valueOf(passwordField.getPassword());
+                user = getAuthenicationUser(id,pass);
+                if(user)
+                    dispose();
+                else
+                {
+                    JOptionPane.showMessageDialog(LogIn.this,"ID or password Invalid","try again",JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        setVisible(true);
+    }
+    private boolean getAuthenicationUser(String id, String pass) {
+        final String DB_Url = "jdbc:mysql://localhost/reservation?serverTimezone=UTC";
+        final String Username = "root";
+        final String Pass ="T#9758@qlph";
+        try
+        {
+            Connection con= DriverManager.getConnection(DB_Url,Username,Pass);
+            String query = "SELECT 1 FROM customer WHERE NATIONALID2 = ? AND PASSWORD = ?  LIMIT 1";
+            try (PreparedStatement statement = con.prepareStatement(query)) {
+                statement.setString(1, id);
+                statement.setString(2,pass);
+                try (ResultSet rs = statement.executeQuery()) {
+                    return rs.next(); // If there is a matching row, the login is valid; otherwise, it's invalid.
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+        LogIn LogForm = new LogIn(null);
+        if(LogForm.user )
+        {
+            System.out.println("log in successfully\n");
+        }
+        else
+        {
+            System.out.println("logged in canceled");
+        }
+    }
+}
---
+++ Airline/src/Registeration.java
@@ -0,0 +1,112 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class Registeration extends JDialog{
+    private JTextField TKID;
+    private JTextField TFName;
+    private JTextField TFEmail;
+    private JTextField TFAge;
+    private JButton registerButton;
+    private JButton cancelButton;
+    private JPanel RegisterationForm;
+    private JPanel label;
+    private JPasswordField passwordField;
+    private JPasswordField CONpasswordField;
+    private JTextField Confirm;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+    public Registeration(JFrame parent) {
+        super(parent);
+        setTitle("Welcome :) ");
+        setContentPane(RegisterationForm);
+        setMinimumSize(new Dimension(550, 474));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                RegisterUser();
+            }
+        });
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        setVisible(true);
+    }
+    public boolean EmailValidator(String email){
+        String strongEmailRegex ="^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        return email.matches(strongEmailRegex);
+    }
+    public boolean RegisterUser;
+    private void RegisterUser() {
+        String id=TKID.getText();
+        String name=TFName.getText();
+        String Password=String.valueOf(passwordField.getPassword());
+        String confirm=String.valueOf(CONpasswordField.getPassword());
+        String Email=TFEmail.getText();
+        String Age=TFAge.getText();
+        if(id.isEmpty() || Email.isEmpty() || Password.isEmpty() || name.isEmpty() || Age.isEmpty())
+        {
+            JOptionPane.showMessageDialog(this,"please enter all fields","try again",JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        if(!EmailValidator(Email))
+        {
+            JOptionPane.showMessageDialog(this,"please enter valid Email","try again",JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        if(!Password.equals(confirm))
+        {
+            JOptionPane.showMessageDialog(this,"Confirm Password doesn't match password","try again",JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        RegisterUser=addUserToDataBase(id,name,Age,Password,Email);
+    }
+
+    private boolean addUserToDataBase(String id, String name, String age, String password, String email) {
+        final String DB_Url = "jdbc:mysql://localhost/reservation?serverTimezone=UTC";
+        final String Username = "root";
+        final String Pass ="T#9758@qlph";
+        try
+        {
+            Connection con= DriverManager.getConnection(DB_Url,Username,Pass);
+            String query = "INSERT INTO CUSTOMER (NATIONALID2, USERNAME, AGE, PASSWORD, EMAIL) VALUES (?, ?, ?, ?, ?)";
+            try (PreparedStatement statement = con.prepareStatement(query)) {
+                statement.setString(1, id);
+                statement.setString(2, name);
+                statement.setString(3, age);
+                statement.setString(4, password);
+                statement.setString(5, email);
+                return statement.executeUpdate()>0;
+
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args)
+    {
+        Registeration myForm = new Registeration(null);
+        if(myForm.RegisterUser)
+            System.out.println("successful registeration \n");
+        else
+            System.out.println("Registeration Canceled\n");
+    }
+}
